7359556c216cd91b3afc41f25e3e094cc99da6b0
im-devendhar <devendharyadav781@gmail.com>
2025-12-11 11:28:00 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
+    DB_HOST : 'database-1.c0lqmu6skjv4.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin1234',
     DB_DATABASE : ''
 });
